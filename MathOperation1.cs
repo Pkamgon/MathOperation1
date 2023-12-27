775c88330535535fe42b540acfb906d7789b7261
Pkamgon <105408332+Pkamgon@users.noreply.github.com>
2023-12-26 18:58:44 -0700

    Add files via upload

--- MathOperation1.cs
+++ MathOperation1.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace MathOperation1
 {
-    class MathOperation1
+    public class MathOperation1
     {
         //Create a method that takes two integers as parameters
         //Make one parameter optional by providing a default value
         public int mathOp1(int i, int j = 1)
         {
             //Perform multiplication on integers and return the result
-            int num1 = i;
-            int num2 = j;
+            int num = i;
+            int numX = j;
             int result = i * j;
             return result;
         }
--- Program.cs
+++ Program.cs
@@ -10,29 +10,29 @@ namespace MathOperation1
     {
         static void Main(string[] args)
         {
-            ////Instantiate MathOperation1 class
-            //MathOperation1 mathOp1 = new MathOperation1();
-            ////ask the user to provide first parameter
-            //Console.WriteLine("Enter an integer: ");
-            //int num1 = Convert.ToInt32(Console.ReadLine());
-            ////Ask the user to provide a second parameter or press enter
-            //Console.WriteLine("Enter a second integer, or just press enter: ");
-            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //Instantiate MathOperation1 class
+            MathOperation1 mathOp1 = new MathOperation1();
+            //ask the user to provide first parameter
+            Console.WriteLine("Enter an integer: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            //Ask the user to provide a second parameter or press enter
+            Console.WriteLine("Enter a second integer, or just press enter: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
             try
             {
                 //Call method with 2 parameters if both parameters are provided
                 Console.WriteLine("Enter an integer");
-                int num1 = Convert.ToInt32(Console.ReadLine());
+                int num = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Enter a second integer");
-                int num2 = Convert.ToInt32(Console.ReadLine());
-                int results = mathOp1.MathOperation(num1, num2);
+                int numX = Convert.ToInt32(Console.ReadLine());
+                int results = mathOp1.MathOperation(num, numX);
                 Console.WriteLine(num1 + " * " + num2 + " = " + results);
             }
             catch 
             { 
                 //Call method with one parameter if only one is provided
-                int results = mathOp1.MathOperation("num1");
-                Console.WriteLine("num1" + " * Default 1 = " + results);
+                int results = mathOp1.MathOperation("num");
+                Console.WriteLine("num" + " * Default 1 = " + results);
             }
             Console.ReadLine();
         }
